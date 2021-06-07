791bb301ea8cfea2fc42aa6b2356d05f61a98aff
MarcosPenna <marcoskpo_penpun@hotmail.com>
2021-06-06 23:44:37 -0300

    Adding button with middleware to show products

--- src/store/store.js
+++ src/store/store.js
@@ -1,37 +1,41 @@
-import {createStore} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import reducer from '../reducer/reducer'
+import {fetchMiddleware} from '../middlewares/fetchMiddleware'
 
 const defaultData = {
     products: [
-        {
-            id: 1,
-            name: "IPA",
-            description: "La birra se toma fria",
-            image: "https://picsum.photos/300/200?1",
-            price: 400
-        },
-        {
-            id: 2,
-            name: "Stout",
-            description: "La birra se toma fria",
-            image: "https://picsum.photos/300/200?2",
-            price: 300
-        },
-        {
-            id: 3,
-            name: "Golden",
-            description: "La birra se toma fria",
-            image: "https://picsum.photos/300/200?3",
-            price: 100
-        },
-        {
-            id: 4,
-            name: "Red",
-            description: "La birra se toma fria",
-            image: "https://picsum.photos/300/200?4",
-            price: 200
-        }
+        // {
+        //     id: 1,
+        //     name: "IPA",
+        //     description: "La birra se toma fria",
+        //     image: "https://picsum.photos/300/200?1",
+        //     price: 400
+        // },
+        // {
+        //     id: 2,
+        //     name: "Stout",
+        //     description: "La birra se toma fria",
+        //     image: "https://picsum.photos/300/200?2",
+        //     price: 300
+        // },
+        // {
+        //     id: 3,
+        //     name: "Golden",
+        //     description: "La birra se toma fria",
+        //     image: "https://picsum.photos/300/200?3",
+        //     price: 100
+        // },
+        // {
+        //     id: 4,
+        //     name: "Red",
+        //     description: "La birra se toma fria",
+        //     image: "https://picsum.photos/300/200?4",
+        //     price: 200
+        // }
     ]
 }
 
-export default createStore(reducer, defaultData);
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const composedEnhancer = compose(applyMiddleware(fetchMiddleware), reduxDevTools)
+
+export default createStore(reducer, defaultData, composedEnhancer);
